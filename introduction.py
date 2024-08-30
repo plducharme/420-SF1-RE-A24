47519ceca60757c56ec5df04867e53a7dac3a56a
Pier Luc Ducharme <dutch80@gmail.com>
2024-08-30 17:11:37 -0400

    ajout exemples introduction

---
+++ introduction.py
@@ -0,0 +1,26 @@
+# Bases de Python
+
+# Assignation de la valeur 5 à la variable valeur_a
+valeur_a = 5
+
+# Assignation de la chaîne de caractères entrée par l'utilisateur à la variable entree
+entree = input("Entrez un nombre: ")
+
+# Impression / affichage
+print("Bonjour")
+print(23)
+
+# Types de données de bases
+print("=== Types de données ===")
+# Entier (int)
+print(type(42))
+# Décimal (float)
+print(type(42.0))
+# Chaîne de caractères (str)
+print(type("42"))
+# Booléen (bool)
+print(2 == 2)
+print(True)
+print(False)
+
+
---
+++ opérateurs.py
@@ -0,0 +1,30 @@
+print("=== Opérateurs ===")
+nombre = 10
+# addition
+print(nombre + 5)
+# multiplication
+print(nombre * 5)
+# division
+print(nombre / 2)
+# soustraction
+print(nombre - 2)
+# modulo
+print(nombre % 2)
+# puissance
+print(nombre ** 2)
+# division entière (plancher de la division)
+print(nombre // 2)
+
+# La priorité des opérateurs est similaire à celle des mathématiques. Les opérations entre parenthèses sont évaluées
+# en premier, suivies des opérations de puissance, de multiplication et de division, et enfin des opérations
+# d'addition et de soustraction. Les opérations de même priorité sont évaluées de gauche à droite.:
+print("=== Priorité ===")
+print(4 * 2 + 3)
+print(4 * (2 + 3))
+
+# Nombres
+print("=== Nombres ===")
+print(4E15)
+print(1.025)
+print(42)
+print(-4.7e-4)
